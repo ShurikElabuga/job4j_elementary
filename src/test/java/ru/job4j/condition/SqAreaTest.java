01e2942a99bb5250cf6a8062c86dea3f34f1b936
Aleksandr <ShurikElabuga@yandex.ru>
2023-09-10 16:29:51 +0300

    4.6.2. Switch - matcher [#334071]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -8,7 +8,7 @@ class SqAreaTest {
 
     @Test
     void whenP6K2Square2() {
-        int expected = 4;
+        int expected = 2;
         int p = 6;
         double k = 2;
         double out = SqArea.square(p, k);
