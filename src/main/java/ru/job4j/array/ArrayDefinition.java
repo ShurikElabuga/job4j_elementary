03d036a2276dbcc210d43fd78419078bf2f0a887
Aleksandr <ShurikElabuga@yandex.ru>
2023-09-14 20:57:27 +0300

    6.0.3. Заполнение массива. [#226300 #445193]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,6 +8,10 @@ public class ArrayDefinition {
         names[1] = "Piter Pen";
         names[2] = "Nikola Tesla";
         names[3] = "Joseph Biden";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
 
     }
 }
