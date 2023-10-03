1c72ef962e553b633e64ea1a9a2febe246bfb847
Aleksandr <ShurikElabuga@yandex.ru>
2023-10-03 22:50:11 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,4 +5,12 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int middle, int right) {
+        return max(left, max(middle, right));
+    }
+
+    public static int max(int left, int middleLeft, int middleRight, int right) {
+        return max(left, max(middleLeft, middleRight, right));
+    }
+
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -32,4 +32,37 @@ class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax1To2To3To4() {
+        int left = 1;
+        int middleLeft = 2;
+        int middleRight = 3;
+        int right = 4;
+        int result = Max.max(left, middleLeft, middleRight, right);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax4Equals4And4And4() {
+        int left = 4;
+        int middleLeft = 4;
+        int middleRight = 4;
+        int right = 4;
+        int result = Max.max(left, middleLeft, middleRight, right);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax4To3To2To1() {
+        int left = 4;
+        int middleLeft = 3;
+        int middleRight = 2;
+        int right = 1;
+        int result = Max.max(left, middleLeft, middleRight, right);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
 }
